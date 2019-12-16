6940d430afd7891f0ad84038c809305e8749ee23
Grady DeReamer <gradydereamer@Gradys-MBP.cmts.mlf.ptd.net>
2019-12-15 19:04:53 -0500

    new

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
-var ul = document.getElementById("myUl");
+var tasks = [];
 
-function add(){
-     var tasks = document.getElementById("todo-input").value;
-     var taskText = document.createTextNode(tasks);
-     var li = document.createElement("li");
-     var button = document.createElement("button");
-     var buttonDelete = document.createTextNode("x");
-     button.setAttribute("onclick", "remove()");
-     button.appendChild(buttonDelete);
-     li.appendChild(taskText);
-     li.appendChild(button);
-     ul.appendChild(li);
-     localStorage["list"] = ul.innerHTML
-}
+$("#add").click(function(){
+    var task = $("#task").val();
+    var btn = $('<button/>',
+    {
+        text:"Done",
+        value: task,
+        click: function (){
+            alert('clicked');
+        }
+    });
+    $("#tasks").append('<li>');
+    $("#tasks").append(task);
+    $("#tasks").append(btn);
+    $("#tasks").append('</li>');
+    task.push(task);
+});
 
-function remove(){
-    var task = this.event.currentTarget.parentNode;
-    ul.removeChild(task);
-    localStorage["list"] = ul.innerHTML
-}
-if (localStorage["list"]){
-    ul.innerHTML = localStorage["list"];
-}
+$("#clear").click(function(){
+    $("#tasks").empty();
+    tasks = [];
+});
