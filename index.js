65f1c564869bbdce824d4937365590f45395aca3
Grady DeReamer <gradydereamer@Gradys-MacBook-Pro.local>
2019-12-16 15:26:17 -0500

    It finally Works

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 var tasks = [];
-
-$("#add").click(function(){
+$(document).ready(function() {
+  $("#add").click(function() {
     var task = $("#task").val();
-    var btn = $('<button/>',
-    {
-        text:"Done",
-        value: task,
-        click: function (){
-            alert('clicked');
-        }
+    var btn = $("<button/>", {
+      text: "Done",
+      value: task,
+      click: function() {
+        alert("clicked");
+      }
     });
-    $("#tasks").append('<li>');
+
+    console.log("Button was clicked!");
+
+    $("#tasks").append("<li>");
     $("#tasks").append(task);
     $("#tasks").append(btn);
-    $("#tasks").append('</li>');
-    task.push(task);
-});
+    $("#tasks").append("</li>");
+    // task.push(task);
+  });
 
-$("#clear").click(function(){
+  $("#clear").click(function() {
     $("#tasks").empty();
     tasks = [];
+  });
 });
